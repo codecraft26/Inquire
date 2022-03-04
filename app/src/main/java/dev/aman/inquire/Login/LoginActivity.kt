6f2c20dfed58a1b0d3dda1a01c9515879d9e1b75
codecraft26 <codescraft26@gmail.com>
2022-03-04 15:28:09 +0530

    Initial commit

---
+++ app/src/main/java/dev/aman/inquire/Login/LoginActivity.kt
@@ -0,0 +1,14 @@
+package dev.aman.inquire.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.aman.inquire.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+
+    }
+}
