ff42ec76eb943113c11f0012d2aedf55e76f2e17
codecraft26 <codescraft26@gmail.com>
2022-03-04 15:42:27 +0530

    final set of splash screen

--- app/src/main/java/dev/aman/inquire/splashScreen/SplashActivity.kt
+++ app/src/main/java/dev/aman/inquire/splashScreen/SplashActivity.kt
@@ -1,11 +1,25 @@
 package dev.aman.inquire.splashScreen
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import dev.aman.inquire.Login.LoginActivity
+import dev.aman.inquire.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var  binding:ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
+        binding= ActivitySplashBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        Handler().postDelayed({
+            val mIntent = Intent(this@SplashActivity, LoginActivity::class.java)
+            startActivity(mIntent)
+            finish()
+        }, 2000)
 
     }
 }
