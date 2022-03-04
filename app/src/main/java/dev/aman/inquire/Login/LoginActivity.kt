f80d02448e9a7aa348efac1531fae86e8589a606
codecraft26 <codescraft26@gmail.com>
2022-03-04 15:54:49 +0530

    adding login activity

--- app/src/main/java/dev/aman/inquire/Login/LoginActivity.kt
+++ app/src/main/java/dev/aman/inquire/Login/LoginActivity.kt
@@ -3,11 +3,14 @@ package dev.aman.inquire.Login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.aman.inquire.R
+import dev.aman.inquire.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
 
 
     }
