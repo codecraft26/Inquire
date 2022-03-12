7d7f620d924be2ef029ca716489089a95e3a227b
codecraft26 <codescraft26@gmail.com>
2022-03-13 01:06:05 +0530

    setting acitivity for organisation and individual

--- app/src/main/java/dev/aman/inquire/splashScreen/SplashActivity.kt
+++ app/src/main/java/dev/aman/inquire/splashScreen/SplashActivity.kt
@@ -4,8 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
-import dev.aman.inquire.Login.LoginActivity
 import dev.aman.inquire.databinding.ActivitySplashBinding
+import dev.aman.inquire.organisation.SelectionActivity
 
 class SplashActivity : AppCompatActivity() {
     private lateinit var  binding:ActivitySplashBinding
@@ -16,7 +16,7 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
         Handler().postDelayed({
-            val mIntent = Intent(this@SplashActivity, LoginActivity::class.java)
+            val mIntent = Intent(this@SplashActivity, SelectionActivity::class.java)
             startActivity(mIntent)
             finish()
         }, 9000)
