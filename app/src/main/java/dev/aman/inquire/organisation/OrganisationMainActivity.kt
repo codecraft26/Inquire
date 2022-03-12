8d67ea6d603040aea8e953f29bc85f4b2412eadc
codecraft26 <codescraft26@gmail.com>
2022-03-13 01:58:26 +0530

    orgnisation main actitvity

---
+++ app/src/main/java/dev/aman/inquire/organisation/OrganisationMainActivity.kt
@@ -0,0 +1,18 @@
+package dev.aman.inquire.organisation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import dev.aman.inquire.R
+import dev.aman.inquire.databinding.ActivityOrganisationMainBinding
+import dev.aman.inquire.databinding.ActivityOrganisationSplashBinding
+
+class OrganisationMainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityOrganisationMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding= ActivityOrganisationMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
