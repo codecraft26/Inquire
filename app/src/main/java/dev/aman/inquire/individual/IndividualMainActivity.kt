094e83880fdcf40e79f5d973f8b701b9f0587c65
codecraft26 <codescraft26@gmail.com>
2022-04-11 11:32:19 +0530

    some changes in IndividualMainActivity.kt

--- app/src/main/java/dev/aman/inquire/individual/IndividualMainActivity.kt
+++ app/src/main/java/dev/aman/inquire/individual/IndividualMainActivity.kt
@@ -2,8 +2,17 @@ package dev.aman.inquire.individual
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.system.Os.close
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 
 import dev.aman.inquire.R
 import dev.aman.inquire.individual.ui.bookmarked.BookmarkedFragment
@@ -12,6 +21,7 @@ import dev.aman.inquire.individual.ui.Home.HomeFragment
 
 class IndividualMainActivity : AppCompatActivity() {
     private lateinit var currentFragment : Fragment
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_individual_main)
@@ -20,11 +30,6 @@ class IndividualMainActivity : AppCompatActivity() {
         ).commit()
         val bottomNav : BottomNavigationView = findViewById(R.id.bottom_navView)
         bottomNav.setOnNavigationItemSelectedListener(bottomListener)
-
-
-
-
-
 
     }
     val bottomListener = BottomNavigationView.OnNavigationItemSelectedListener {
