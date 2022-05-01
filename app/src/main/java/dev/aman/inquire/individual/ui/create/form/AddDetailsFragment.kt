a1a5e8067dea456d23414be285c04d4e26036a1f
codecraft26 <codescraft26@gmail.com>
2022-05-01 14:56:47 +0530

    add details fragment making working

--- app/src/main/java/dev/aman/inquire/individual/ui/create/form/AddDetailsFragment.kt
+++ app/src/main/java/dev/aman/inquire/individual/ui/create/form/AddDetailsFragment.kt
@@ -1,25 +1,68 @@
 package dev.aman.inquire.individual.ui.create.form
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import dev.aman.inquire.R
+import android.widget.Toast
+import androidx.core.widget.doOnTextChanged
+import androidx.fragment.app.Fragment
+import dev.aman.inquire.databinding.FragmentAddDetailsBinding
+import dev.aman.inquire.individual.ui.create.CreateFragment
+
+
 class AddDetailsFragment : Fragment() {
-
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
-
+    private lateinit var binding: FragmentAddDetailsBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_add_details, container, false)
+        binding = FragmentAddDetailsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        loadView()
+
     }
 
+    private fun loadView() {
+        binding.textFieldSnippetTitle.editText?.doOnTextChanged { text, _, _, _ ->
+            binding.textFieldSnippetTitle.error = if (text.isNullOrBlank()) {
+                "Title is required"
+            } else {
+                null
+            }
+        }
 
+        binding.textFieldSnippetDescription.editText?.doOnTextChanged { text, _, _, _ ->
+            binding.textFieldSnippetDescription.error = if (text.isNullOrBlank()) {
+                "Description is required"
+            } else {
+                null
+            }
+
+        }
+
+
+        binding.textFieldSnippetTags.editText?.doOnTextChanged { text, _, _, _ ->
+            binding.textFieldSnippetTags.error = if (text.isNullOrBlank()) {
+                "Language  are required"
+            } else {
+                null
+            }
+        }
+        binding.addDetailsButtonNext.setOnClickListener{
+           if (binding.textFieldSnippetTitle.editText?.text.isNullOrBlank() ||
+               binding.textFieldSnippetDescription.editText?.text.isNullOrBlank() ||
+               binding.textFieldSnippetTags.editText?.text.isNullOrBlank()
+           ){
+               Toast.makeText(context,"Please fill all the fields",Toast.LENGTH_SHORT).show()
+           }else{
+               (parentFragment as? CreateFragment)?.nextPage()
+
+           }
+        }
+    }
 }
+
