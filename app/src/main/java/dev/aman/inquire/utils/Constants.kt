4c96a292a5b828964858f67e5cbd367e97fc41a3
codecraft26 <codescraft26@gmail.com>
2022-05-02 17:04:33 +0530

    adding constants value for input dioluge box

--- app/src/main/java/dev/aman/inquire/utils/Constants.kt
+++ app/src/main/java/dev/aman/inquire/utils/Constants.kt
@@ -24,4 +24,11 @@ object Constants {
         "Elastcic",
         "Machine Learning"
     )
+    const val KEY_INPUT_CODE_DIALOG = "key_input_code_dialog"
+
+    const val KEY_CODE = "key_code"
+    const val KEY_PUBLISHED_FIRST_SNIPPET = "key_published_first_snippet"
+
+    const val KEY_USER_PREFERRED_TAGS = "user_preferred_tags"
+    const val KEY_USER = "shared_pref_user"
 }
