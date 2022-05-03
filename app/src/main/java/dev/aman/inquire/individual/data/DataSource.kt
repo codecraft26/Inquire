cd44d9d530ab456ed390daf4c567f663fa46043d
codecraft26 <codescraft26@gmail.com>
2022-05-03 22:08:32 +0530

    data source file

---
+++ app/src/main/java/dev/aman/inquire/individual/data/DataSource.kt
@@ -0,0 +1,55 @@
+package dev.aman.inquire.individual.data
+
+import dev.aman.inquire.R
+
+class DataSource {
+fun getInquire():List<Inquire>
+{
+    return listOf<Inquire>(
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        ),
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        ),
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        ),
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        ),
+
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        ),
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        ),
+        Inquire("How to add diffrent view holder in android recycler view",
+            "how to manage diffrent view holder in android recycler view  we use the alignment how to man" +
+                    "that type of reosurces in any kind of recyclerviewer" +
+                    "",
+            R.drawable.img,R.drawable.ic_about
+        )
+    )
+}
+
+}
