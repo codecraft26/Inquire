c508c5e62aa840fc9e7f912e1dfdc80cc91090ea
codecraft26 <codescraft26@gmail.com>
2022-05-16 22:15:43 +0530

    clean up the code

--- app/src/main/java/dev/aman/inquire/individual/data/model/Inquire.kt
+++ app/src/main/java/dev/aman/inquire/individual/data/model/Inquire.kt
@@ -1,16 +1,14 @@
 package dev.aman.inquire.individual.data.model
 
 import android.net.Uri
-import com.google.firebase.firestore.DocumentSnapshot
 import dev.aman.inquire.utils.InquireState
 
 data class Inquire(
-    var id: Int = 0,
-    val title:String="",
+    var title:String="",
     val description:String="",
     val inquire_code:String="",
     val language:String="",
-    val timestamp: String? = null,
+    /*val timestamp: String? = null,*/
     /*val createdBy: Task<DocumentSnapshot>? = null,*/
     val imageUri: Uri? = null,
     var status: InquireState = InquireState.UnBookmarked,
